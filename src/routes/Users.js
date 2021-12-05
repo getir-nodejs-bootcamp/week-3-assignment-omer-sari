1f74453083e863efbca850e01b6742c6ed8a0aaa
omeersari <omeersari@gmail.com>
2021-12-05 17:23:25 +0300

    fix

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -10,17 +10,4 @@ router.route("/deleteuser").delete(deleteUser)
 router.route("/updateuser").patch(updateUser)
 router.route("/updateusers").put(updateUser)
 
-
-
-
-
-
-/*
-router.get("/get-user", verifyAuthToken, userController.getUser);
-router.post("/add-user", verifyAuthToken, userController.addUser);
-router.delete("/delete-user", verifyAuthToken, userController.deleteUser);
-router.patch("/update-user", verifyAuthToken, userController.updateUser);
-router.put("/change-user", verifyAuthToken, userController.changeUser);
-*/
-
 module.exports = router;
